36cfa4e89a93eade0e1a4f51fd171ae160c6d122
Yash Raj Dwivedi <dwivedi.raj.yash@gmail.com>
2025-07-09 07:19:56 +0530

    db access tool done

--- agent/tools_utils/db_utils.py
+++ agent/tools_utils/db_utils.py
@@ -12,7 +12,7 @@ def perform_db_actions(parameter: dict):
         with _driver.session() as session:
             results = session.run(query).data()
             print("DB Results are",results)
-            if results:
+            if parameter["comparison"]!= "Not Applicable":
                 if parameter["comparison"] == "equal_to":
                     if results[0]['n']['balance'] == parameter['amount']:
                         print("DB Results:")
@@ -20,7 +20,44 @@ def perform_db_actions(parameter: dict):
                     else:
                         print("DB Results:")
                         print(f"{parameter['name']} does not have balace {parameter['amount']}") 
-                        return(f"{parameter['name']} does not have balace {parameter['amount']}")   
+                        return(f"{parameter['name']} does not have balace {parameter['amount']}")  
+                elif parameter["comparison"] == "greater_than":
+                    if results[0]['n']['balance'] > parameter['amount']:
+                        print("DB Results:")
+                        return(f"Yes {parameter['name']} has balance greater than {parameter['amount']}")
+                    else:
+                        print("DB Results:")
+                        print(f"{parameter['name']} does not have balance greater than {parameter['amount']}") 
+                        return(f"{parameter['name']} does not have balance greater than {parameter['amount']}")
+                elif parameter["comparison"] == "less_than":
+                    if results[0]['n']['balance'] < parameter['amount']:
+                        print("DB Results:")
+                        return(f"Yes {parameter['name']} has balance less than {parameter['amount']}")
+                    else:
+                        print("DB Results:")
+                        print(f"{parameter['name']} does not have balance less than {parameter['amount']}") 
+                        return(f"{parameter['name']} does not have balance less than {parameter['amount']}") 
+                elif parameter["comparison"] == "not_equal_to":
+                    if results[0]['n']['balance'] != parameter['amount']:
+                        print("DB Results:")
+                        return(f"Yes {parameter['name']} has balance not equal to {parameter['amount']}")
+                    else:
+                        print("DB Results:")
+                        print(f"{parameter['name']} has balance equal to {parameter['amount']}") 
+                        return(f"{parameter['name']} has balance equal to {parameter['amount']}")
+            else:
+                if results:
+                    print("DB Results:")
+                    return(f"The records for  {parameter['name']} are {results[0]['n']['balance']}")
+                else:
+                    print("DB Results:")
+                    return(f"No {parameter['name']} found in the database")
+    else:
+        query = f"CREATE (n:{parameter['node_type']} {{name: '{parameter['name']}', balance: {parameter['amount']}}})"
+        with _driver.session() as session:
+            session.run(query)
+            print("DB Results:")
+            print(f"Created {parameter['node_type']} node with name {parameter['name']} and balance {parameter['amount']}")
 
 
 
